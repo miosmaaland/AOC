594d80e33ab9c77a8c23f40bdee46838b0381199
miosmaaland <116658805+miosmaaland@users.noreply.github.com>
2023-12-03 07:08:30 +0100

    Add files via upload

---
+++ 3_december.py
@@ -0,0 +1,36 @@
+import re
+from collections import defaultdict
+
+totalt = 0
+brett = []
+gir_numre = defaultdict(list)
+
+def vurder_tallnaboer(start_y, start_x, end_y, end_x, num):
+    global gir_numre
+    for y in range(start_y, end_y+1):
+        for x in range(start_x, end_x+1):
+            if 0 <= y < len(brett) and 0 <= x < len(brett[y]):
+                if brett[y][x] not in '0123456789.':
+                    if brett[y][x] == '*':
+                        gir_numre[(y, x)].append(num)
+                    return True
+    return False
+
+tall_mønster = re.compile('\d+')
+
+with open('dec3') as fil:
+    for linje in fil.readlines():
+        brett.append(linje.strip())
+
+for rad_num in range(len(brett)):
+    for match in re.finditer(tall_mønster, brett[rad_num]):
+        if vurder_tallnaboer(rad_num-1, match.start()-1, rad_num+1, match.end(), int(match.group(0))):
+            totalt += int(match.group(0))
+
+print(totalt)
+
+totalt = 0
+for k, v in gir_numre.items():
+    if len(v) == 2:
+        totalt += v[0] * v[1]
+print(totalt)
